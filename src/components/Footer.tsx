4c09c98085795ec410ffcddae73ff2d9269d06dc
FelipeOropeza <felipe2006.co@gmail.com>
2025-05-03 23:07:30 -0300

    feat: adição do footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,57 @@
+function Footer() {
+  return (
+    <footer className="footer sm:footer-horizontal bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-200 items-center p-4 border-t border-gray-200 dark:border-gray-700">
+      <aside className="grid-flow-col items-center">
+        <svg
+          width="36"
+          height="36"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          className="fill-current"
+        >
+          <path d="M22.672 15.226l-2.432.811...z"></path>
+        </svg>
+        <p>Copyright © {new Date().getFullYear()} - All rights reserved</p>
+      </aside>
+      <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+        <a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            className="fill-current"
+          >
+            <path d="M24 4.557c-.883...z"></path>
+          </svg>
+        </a>
+        <a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            className="fill-current"
+          >
+            <path d="M19.615 3.184c-3.604...z"></path>
+          </svg>
+        </a>
+        <a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            className="fill-current"
+          >
+            <path d="M9 8h-3v4h3v12h5v-12h3.642...z"></path>
+          </svg>
+        </a>
+      </nav>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
+import Footer from "./Footer";
 
 const Layout = () => {
   return (
     <div>
-        <NavBar />
+      <NavBar />
       <main>
         <Outlet />
       </main>
-      <footer>Meu Footer</footer>
+      <Footer />
     </div>
   );
 };
